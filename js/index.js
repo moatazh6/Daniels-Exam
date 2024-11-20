407eaef8c723b27bdfa3bd1512d96370d3cd5130
moatazh6 <moatazmoata56@gmail.com>
2024-11-20 15:58:56 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#element", {
+  strings: ["Developer", "Designer", " Larry Daniels"],
+
+  typeSpeed: 100,
+  backSpeed: 100,
+
+  loop: true,
+  loopCount: Infinity,
+});
+
+
+
